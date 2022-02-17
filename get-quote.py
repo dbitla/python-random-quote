f90c2988ff5869d0eaaac69fbc2836feed3dde31
dbitla <dbitla01@mynbcc.ca>
2022-02-17 03:43:18 -0400

    New change is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 import random
 def start():
-	print("Keep it logically awesome.")
+	#print("Keep it logically awesome.")
 
 	f = open("quotes.txt")
 	quotes = f.readlines()
 	f.close()
-	last = 13
+	last = 20
 	rnd = random.randint(0, last)
 	print(quotes[rnd])
 
